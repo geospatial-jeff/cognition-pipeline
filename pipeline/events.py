b8052413e967db7bcffed5282b18a7a1dc24d922
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2018-12-17 09:19:55 -0700

    updating events

--- pipeline/events.py
+++ pipeline/events.py
@@ -6,7 +6,8 @@ def invoke(f):
     @wraps(f)
     def wrapper(self, event, context):
         return f(self, event, context)
-    wrapper.id = "lambda"
+    wrapper.trigger = "lambda"
+    wrapper.args = {}
     return wrapper
 
 def http(path, method, cors):
@@ -16,7 +17,7 @@ def http(path, method, cors):
         def wrapped_f(self, event, context):
             data = json.loads(event['body'])
             return f(self, data, context)
-        wrapped_f.id = 'http'
+        wrapped_f.trigger = 'http'
         wrapped_f.args = {'path': path, 'method': method, 'cors': cors}
         return wrapped_f
     return wrapper
@@ -28,8 +29,8 @@ def sns(resource):
         def wrapped_f(self, event, context):
             data = json.loads(event['Records'][0]['Sns']['Message'])
             return f(self, data, context)
-        wrapped_f.id = 'sns'
-        wrapped_f.args = {'arn': resource.arn, 'topic_name': resource.name}
+        wrapped_f.trigger = 'sns'
+        wrapped_f.args = {'arn': resource.arn, 'topic_name': resource.name, 'func_name': f.__name__}
         return wrapped_f
     return wrapper
 
@@ -37,12 +38,12 @@ def sqs(resource):
 
     def wrapper(f):
         @wraps(f)
-        def wraped_f(self, event, context):
+        def wrapped_f(self, event, context):
             for record in event['Records']:
                 data = json.loads(record['body'])
                 output = f(self, data, context)
             return
-        wraped_f.id = 'sqs'
-        wraped_f.args = {'arn': resource.arn, 'url': resource.url, 'queue_name': resource.name}
-        return wraped_f
+        wrapped_f.trigger = 'sqs'
+        wrapped_f.args = {'arn': resource.arn, 'url': resource.url, 'queue_name': resource.name}
+        return wrapped_f
     return wrapper
