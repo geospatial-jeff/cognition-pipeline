80828a2d66fc36c6411d06d51b8f3873e36e1131
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2019-01-03 16:47:52 -0700

    Adding class to handle stack outputs, required for testing

---
+++ pipeline/outputs.py
@@ -0,0 +1,29 @@
+import yaml
+
+
+class Outputs(dict):
+
+    @classmethod
+    def load(cls, fname):
+        with open(fname, 'r') as stream:
+            contents = yaml.load(stream)
+            return cls(contents)
+
+    def __init__(self, contents):
+        super().__init__()
+        self.update(contents)
+
+    def endpoint(self):
+        if 'ServiceEndpoint' in self.keys():
+            return self['ServiceEndpoint']
+        else:
+            return None
+
+    def deployment_bucket(self):
+        return self['ServerlessDeploymentBucketName']
+
+    def functions(self):
+        if 'functions' in self.keys():
+            return self['functions']
+        else:
+            return None
