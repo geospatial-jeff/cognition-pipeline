734459f91fac5a5bd0317cf865ee2f7af680fc77
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2019-01-04 11:07:21 -0700

    removing scratch code

--- pipeline/events.py
+++ pipeline/events.py
@@ -85,6 +85,3 @@ def bucket_notification(bucket, event_type, destination, prefix=None):
         wrapped_f.args = {'bucket': bucket, 'event': event_type, 'destination': destination, 'prefix': prefix}
         return wrapped_f
     return wrapper
-
-
-{"Records":[{"eventVersion":"2.1","eventSource":"aws:s3","awsRegion":"us-east-1","eventTime":"2019-01-04T17:40:07.181Z","eventName":"ObjectCreated:Put","userIdentity":{"principalId":"AWS:AIDAILOGY4ZN4UTP42ZTC"},"requestParameters":{"sourceIPAddress":"67.173.242.173"},"responseElements":{"x-amz-request-id":"C3481794F6CEFEDE","x-amz-id-2":"1nTYNl8Ogh1ncaK3c1o4qbKt4/8NrtdYOxb/u9PqEz1mNbB0zdOpm1xh9x11+N0qWCQBGXmSyDU="},"s3":{"s3SchemaVersion":"1.0","configurationId":"b9c99090-c658-4a0a-b79f-197e05c7c38a","bucket":{"name":"cognitionpipelinetests3","ownerIdentity":{"principalId":"AV5BH9QRC4DDJ"},"arn":"arn:aws:s3:::cognitionpipelinetests3"},"object":{"key":"sqs/notification.txt","size":14,"eTag":"ce114e4501d2f4e2dcea3e17b546f339","sequencer":"005C2F9A77119857B3"}}}]}
